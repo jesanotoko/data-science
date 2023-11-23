64b5950f366d3ea427db174e5a30e9e300a0fa5b
jesanotoko <mjesan454@gmail.com>
2023-11-23 17:14:17 +0600

    Python

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,13 @@
 print("Hello World")
+'''Python code can be run written in script files with a .py extension
+1. `print()` Function: 
+`print()` is built in python function to display output to the console or terminal
+2. String Literal:
+"Hello World" is a string literal enclosed double equates.
+String in Python represent textual data and can contain letters, numbers, symbols or spaces.
+3. Function Invocation:
+`print()` is function call where the string "hello World" is passed as an argument.
+In this context, the string argument is what the 'print()' function will display as output.
+4. Output:
+When you execute `print("Hello world")` , it will output the text "hello world" to the console/terminal.
+'''
